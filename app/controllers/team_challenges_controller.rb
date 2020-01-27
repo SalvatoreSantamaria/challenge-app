c4e95123312a60fa7103f1a49d6186dbabea0f7b
Salvatore Santamaria <sam.santamaria@gmail.com>
2020-01-26 21:02:36 -0800

    Completed challenges dont show done button any mor

--- app/controllers/team_challenges_controller.rb
+++ app/controllers/team_challenges_controller.rb
@@ -7,21 +7,6 @@ class TeamChallengesController < ApplicationController
             #create will hit the db directly
 
 
-            #how to add point total?
-            #do I need to find the challenge.team, which doesn't exist?
-            #team = challenge.find(params[:team_id]) #do I need to create :team_id? it does not exist
-
-            #current users challenge,
-            #current_user.challenges.team_id 
-
-            #table is score, score.team_score stores the points, and team_id: 1 is team_one and team_id: 2 is team_two
-            #probably need to use .update()
-            #team = Team.find(params[:team_id])
-            #team = Team.find(2)
-            #test = team.scores.find(2).team_score #hard coding to test. now do I save or just write this correctly?
-            #can also access with team = Team.find(2).scores.find(2).team_score
-            # test = test + 1
-            # puts test
 
             points = Challenge.find(params[:challenge_id])
             points = points.challenge_points
@@ -29,16 +14,17 @@ class TeamChallengesController < ApplicationController
             #current_team_two_score = Team.find(2).scores.find(2).team_score #manual hard code, this works
             puts current_user.id, "*******************"
             #puts Team.find(current_user.id).scores.find(1).team_score, 'Team.find(current_user.id).scores.find(1).team_score '
-            #here is where it fails rd::RecordNotFound (Couldn't find Score with 'id'=1 [WHERE "scores"."team_id" = $1]):
             current_team_two_score = Team.find(current_user.id).scores.find(current_user.id).team_score  #unsure if current_user.id is correct - i might just be passing in the user id, and not the team id
             Score.update(current_user.id, team_score: (current_team_two_score + points)) #unsure if current_user.id is correct - i might just be passing in the user id, and not the team id
 
 
             flash[:notice] = "You have completed #{challenge_to_add.name}!"
-            redirect_to root_path
+            puts "challenge.id is #{challenge_to_add.id} and name is #{challenge_to_add.name}"
+            redirect_to team_path(current_user)
         else  
             flash[:notice] = "Something was wrong with the score update"
-            redirect_to root_path
+            puts "challenge_to_add.id is #{challenge_to_add.id} and name is #{challenge_to_add.name}"
+            redirect_to team_path(current_user)
         end
     end
 
